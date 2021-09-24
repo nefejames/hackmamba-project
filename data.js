7132cbdd7398dd481907a67c059848501c8367f4
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2021-09-24 07:57:49 -0700

    cleaning up

--- data.js
+++ data.js
@@ -10,11 +10,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet1.jpg",
     img: "Hackmamba/Images/pet1.jpg",
     video: "/Hackmamba/Videos/pet1.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet1.mp4",
   },
   {
     id: 2,
@@ -24,8 +21,6 @@ const data = [
     price: 250,
     img: "Hackmamba/Images/pet14.jpg",
     video: "/Hackmamba/Videos/pet2.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet2.mp4",
   },
   {
     id: 3,
@@ -33,11 +28,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet3.jpg",
     img: "Hackmamba/Images/pet10.jpg",
     video: "/Hackmamba/Videos/pet3.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet3.mp4",
   },
   {
     id: 4,
@@ -45,11 +37,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet4.jpg",
     img: "Hackmamba/Images/pet4.jpg",
     video: "/Hackmamba/Videos/pet4.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet4.mp4",
   },
   {
     id: 5,
@@ -57,11 +46,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet5.jpg",
     img: "Hackmamba/Images/pet5.jpg",
     video: "/Hackmamba/Videos/pet5.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet5.mp4",
   },
   {
     id: 6,
@@ -69,11 +55,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet6.jpg",
     img: "Hackmamba/Images/pet17.jpg",
     video: "/Hackmamba/Videos/pet6.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet6.mp4",
   },
   {
     id: 7,
@@ -81,11 +64,8 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet7.jpg",
     img: "Hackmamba/Images/pet7.jpg",
     video: "/Hackmamba/Videos/pet7.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet7.mp4",
   },
   {
     id: 8,
@@ -95,8 +75,6 @@ const data = [
     price: 250,
     img: "Hackmamba/Images/pet13.jpg",
     video: "/Hackmamba/Videos/pet8.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet8.mp4",
   },
   {
     id: 9,
@@ -104,11 +82,9 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet9.jpg",
+
     img: "Hackmamba/Images/pet9.jpg",
     video: "/Hackmamba/Videos/pet9.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet9.mp4",
   },
   {
     id: 10,
@@ -116,11 +92,9 @@ const data = [
     baths: 4,
     title: "Ikoyi House",
     price: 250,
-    // img: "https://res.cloudinary.com/nefejames/image/upload/q_auto,f_auto,w_auto,c_thumb,g_auto/Hackmamba/Images/pet10.jpg",
+
     img: "Hackmamba/Images/pet3.jpg",
     video: "/Hackmamba/Videos/pet10.mp4",
-    // video:
-    //   "https://res.cloudinary.com/nefejames/video/upload/Hackmamba/Videos/pet10.mp4",
   },
 ];
 
